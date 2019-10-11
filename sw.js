696401d9d1576668d4f220502435b9f2318a3dd3
rodo <vazquezbenitezrodolfo@gmail.com>
2019-10-11 09:14:30 -0500

    De nuevo se mcamiaban   las rutas del app shell dynamic e inmutable quitando el slash

--- sw.js
+++ sw.js
@@ -6,24 +6,24 @@ const DYNAMIC_CACHE = "dynamic-v1";
 const INMUTABLE_CACHE = "inmutable-v1";
 
 const APP_SHEL = [
-  "index.html",
-  "css/style.css",
-  "img/favicon.ico",
-  "img/avatars/hulk.jpg",
-  "img/avatars/ironman.jpg",
-  "img/avatars/spiderman.jpg",
-  "img/avatars/thor.jpg",
-  "img/avatars/wolverine.jpg",
-  "js/app.js",
-  "js/sw-utils.js"
+  "/twitor/index.html",
+  "/twitor/css/style.css",
+  "/twitor/img/favicon.ico",
+  "/twitor/img/avatars/hulk.jpg",
+  "/twitor/img/avatars/ironman.jpg",
+  "/twitor/img/avatars/spiderman.jpg",
+  "/twitor/img/avatars/thor.jpg",
+  "/twitor/img/avatars/wolverine.jpg",
+  "/twitor/js/app.js",
+  "/twitor/js/sw-utils.js"
 ];
 
 const APP_SHEL_INMUTABLE = [
   "https://fonts.googleapis.com/css?family=Quicksand:300,400",
   "https://fonts.googleapis.com/css?family=Lato:400,300",
   "https://use.fontawesome.com/releases/v5.3.1/css/all.css",
-  "css/animate.css",
-  "js/libs/jquery.js"
+  "/twitor/css/animate.css",
+  "/twitor/js/libs/jquery.js"
 ];
 
 self.addEventListener("install", e => {
