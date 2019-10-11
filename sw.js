3bf60965911cabc2ed70a53ef345f7dc1e013656
rodo <vazquezbenitezrodolfo@gmail.com>
2019-10-11 10:15:20 -0500

    Modificaciones pa iphone x y arriba

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 //import sw-utils
 importScripts("js/sw-utils.js");
 
-const STATIC_CACHE = "static-v3";
-const DYNAMIC_CACHE = "dynamic-v1";
+const STATIC_CACHE = "static-v4";
+const DYNAMIC_CACHE = "dynamic-v2";
 const INMUTABLE_CACHE = "inmutable-v1";
 
 const APP_SHEL = [
@@ -40,7 +40,10 @@ self.addEventListener("install", e => {
 self.addEventListener("activate", e => {
   const respuesta = caches.keys().then(keys => {
     keys.forEach(key => {
-      if (key !== STATIC_CACHE && key.includes("static")) {
+      if (key !== STATIC_CACHE && key.includes('static')) {
+        return caches.delete(key);
+      }
+      if (key !== DYNAMIC_CACHE && key.includes('dynamic')) {
         return caches.delete(key);
       }
     });
